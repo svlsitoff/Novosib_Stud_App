d96161c8ef3a994abe91ed46bf943877b528fbdc
vladimir_sa <vladimir_sa@holdingbp.ru>
2020-01-21 23:24:03 +0800

    Добавление сортировки

--- Models/Student.cs
+++ Models/Student.cs
@@ -10,14 +10,19 @@ namespace App.Models
     public class Student
     {
         public int Id { get; set; }
+        [Display(Name = "Имя")]
         [Required(ErrorMessage = "Не указано имя")]
         public string FirstName { get; set; }
+        [Display(Name = "Отчество")]
         [Required(ErrorMessage = "Не указано отчество")]
         public string MiddleName { get; set; }
+        [Display(Name = "Фамилия")]
         [Required(ErrorMessage = "Не указана фамилия")]
         public string LastNAme { get; set; }
+        [Display(Name = "Пол")]
         [Required(ErrorMessage = "Не указан пол")]
         public string Sex { get; set; }
+        [Display(Name = "Позывной")]
         [StringLength(16, MinimumLength = 5, ErrorMessage = "Длина строки должна быть от 5 до 16 символов")]
         public string CallName { get; set; }
     }
